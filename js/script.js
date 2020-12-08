8d15e4fae47dfae995ff4db99b92271ecd3d6bbd
Mohammed Youssef <m.youssef.soliman@gmail.com>
2020-12-08 20:17:57 +0200

    site finished desktop nav and hero page

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+
+// hero carsoul
+
+$('.owl-carousel').owlCarousel({
+    items:1,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:5000,
+    animateIn: 'fadeIn',
+    animateOut: 'fadeOut',
+    smartSpeed:3000
+});
