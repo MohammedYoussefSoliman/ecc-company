dfcd0529bf6e8c9a445cff7202f7bb25eb92e837
Mohammed Youssef <m.youssef.soliman@gmail.com>
2020-12-10 00:12:21 +0200

    finished header responsive dropdown menu and mobile sidenav

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
 "use strict";
 
+const anchors = document.querySelectorAll('.anchor');
+
+anchors.forEach( anchor => {
+    anchor.addEventListener('click', ()=> {
+        anchor.classList.toggle('arrow__rotate')
+    })
+})
+// mobile nav
+
+const mobileMenu = document.querySelector('.toggle--mobile--nav');
+const closeMenu = document.querySelector('.close--anchor');
+const aside = document.querySelector('.mobile--aside');
+
+mobileMenu.addEventListener('click', (e)=> {
+    e.preventDefault();
+    aside.classList.toggle('open')
+})
+
+closeMenu.addEventListener('click', (e)=> {
+    e.preventDefault();
+    aside.classList.remove('open')
+})
 
 // hero carsoul
 
-$('.owl-carousel').owlCarousel({
+$('.ecc--hero--list').owlCarousel({
     items:1,
     loop:true,
     autoplay:true,
@@ -12,3 +34,8 @@ $('.owl-carousel').owlCarousel({
     animateOut: 'fadeOut',
     smartSpeed:3000
 });
+
+
+// section animation
+
+AOS.init();
